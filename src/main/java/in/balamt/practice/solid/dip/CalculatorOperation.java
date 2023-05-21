f0ee5375fc7caf23b11218317bd940b1051ec6f7
Balamurugan <32746133+balamt@users.noreply.github.com>
2023-05-21 18:27:10 +0530

    Completed SOLID principle example programs

---
+++ src/main/java/in/balamt/practice/solid/dip/CalculatorOperation.java
@@ -0,0 +1,5 @@
+package in.balamt.practice.solid.dip;
+
+public interface CalculatorOperation {
+    int calculate(int a, int b);
+}
---
+++ src/main/java/in/balamt/practice/solid/dip/Subtraction.java
@@ -0,0 +1,8 @@
+package in.balamt.practice.solid.dip;
+
+public class Subtraction implements CalculatorOperation{
+    @Override
+    public int calculate(int a, int b) {
+        return a - b;
+    }
+}
